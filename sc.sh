846649219d81cb59b60813d93e2c7882c787fb89
Xea <0xea@autistici.org>
2023-06-15 14:08:03 +0000

    *non-functioning* sqlite3 rewrite

--- sc.sh
+++ sc.sh
@@ -3,32 +3,47 @@ extract_urls () {
 	curl --max-time 5 "$1" 2>/dev/zero | \
 	grep -o -e "href='\?https\?://[^']\+" -e "href=\"\?https\?://[^\"]\+" | \
 	sed -e "s/href=['\"]\?https\?:\/\///" -e 's/\/$//' -e '/^.\{101,\}$/d' \
-	-e "s/href=['\"]\?https\?:\/\///" -e 's/\/$//' -e '/^.\{101,\}$/d' \
-	> "sites/${2}.log"
+	-e "s/href=['\"]\?https\?:\/\///" -e 's/\/$//' -e '/^.\{101,\}$/d'
 }
 dump_site () {
 	url="$1"
-	file="$(echo "$url" | tr '/' '-')"
-	if ! test -e "sites/${file}.log"; then
-		echo "dumping: $url"
-		extract_urls "$url" "$file" 
-		if [ $? -ne 0 ]; then
-			echo "error: dumping $url failed"
-			rm "sites/${file}.log"
-			return
-		fi
-		cat sites/${file}.log >> all.log
-	fi
-	url="$(echo "$url" | cut -d '/' -f1)"
-	file="$(echo "$url" | tr '/' '-')"
-	test -e "sites/${file}.log" && return
 	echo "dumping: $url"
-	extract_urls "$url" "$file" 
-	cat sites/${file}.log >> all.log
+	output=$(extract_urls "$url")
+	rc=$?
+	if [ $rc -ne 0 ]; then
+		echo "error: dumping $url failed"
+		return
+	fi
+	sqlite3 sql.db "UPDATE urls SET dumped = '1' WHERE id = '${2}';"
+	while IFS= read -r url; do
+		sqlite3 sql.db "INSERT INTO urls (url) VALUES ('${url}');"
+	done <<< "$output"
 }
-mkdir -p dump dump/sites && cd dump
-dump_site "$1"
-timeout 10 tail -n +1 -f all.log | while IFS= read -r site; do
-	dump_site "$site"
+sc_init () {
+	mkdir -p dump dump/sites && cd dump
+	if ! test -e sql.db; then
+		sqlite3 sql.db ""
+		sqlite3 sql.db "CREATE TABLE urls (
+			id INTEGER PRIMARY KEY,
+			url TEXT,
+			dumped BOOLEAN DEFAULT '0',
+			referenced INTEGER DEFAULT '1'
+		);"
+		sqlite3 sql.db "INSERT INTO urls (url, referenced) VALUES ('${1}', '0');"
+	fi
+}
+
+sc_init "$1"
+
+while :
+do
+	echo false > cond
+	sqlite3 -csv sql.db "SELECT id, url FROM urls WHERE dumped = 0;" | while IFS=, read -r id url; do
+		test "$url" == "" && break
+		dump_site "$url" "$id"
+		echo true > cond
+	done
+	test "$(cat cond)" == "false" && break
 done
+
 echo "error: ran out of websites"
