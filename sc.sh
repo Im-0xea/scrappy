9653b820bca96cff71467165339ee899f0f42e57
Xea <0xea@autistici.org>
2023-06-15 14:49:58 +0000

    escape quotes

--- sc.sh
+++ sc.sh
@@ -17,12 +17,13 @@ dump_site () {
 	test "$output" == "" && return
 	sqlite3 sql.db "UPDATE urls SET dumped = '1' WHERE id = '${2}';"
 	while IFS= read -r url; do
-		c=$(sqlite3 sql.db "SELECT count(*) FROM urls WHERE url = '${url}';")
+		furl=$(echo $url | sed "s/'/''/g")
+		c=$(sqlite3 sql.db "SELECT count(*) FROM urls WHERE url = '${furl}';")
 		if test "$c" == "0"; then
-			sqlite3 sql.db "INSERT INTO urls (url) VALUES ('${url}');"
+			sqlite3 sql.db "INSERT INTO urls (url) VALUES ('${furl}');"
 		else
 			c=$(($c + 1))
-			sqlite3 sql.db "UPDATE urls SET referenced = '${c}' WHERE url = '${url}';"
+			sqlite3 sql.db "UPDATE urls SET referenced = '${c}' WHERE url = '${furl}';"
 		fi
 	done <<< "$output"
 }
@@ -32,10 +33,11 @@ sc_init () {
 		sqlite3 sql.db ""
 		sqlite3 sql.db "CREATE TABLE urls (
 			id INTEGER PRIMARY KEY,
-			url TEXT,
+			url TEXT UNIQUE,
 			dumped BOOLEAN DEFAULT '0',
 			referenced INTEGER DEFAULT '1'
 		);"
+		furl=$(echo $1 | sed "s/'/''/g")
 		sqlite3 sql.db "INSERT INTO urls (url, referenced) VALUES ('${1}', '0');"
 	fi
 }
